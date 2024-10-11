1bafc8c2f3fea3711dc33d89e859da7be29814f7
Santiey <santanamuchena@gmail.com>
2024-10-11 15:49:21 +0200

    Updated code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+function updateTime() {
 let mutareElement = document.querySelector("#mutare");
   if (mutareElement) {
     let mutareDateElement = mutareElement.querySelector(".date");
@@ -10,14 +11,39 @@ let mutareElement = document.querySelector("#mutare");
     );
   }
 
-  let munichElement = document.querySelector("#munich");
-  if (munichElement) {
-    let munichDateElement = munichElement.querySelector(".date");
-    let munichTimeElement = munichElement.querySelector(".time");
-    let munichTime = moment().tz("Europe/Munich");
+  let santiagoElement = document.querySelector("#santiago");
+  if (santiagoElement) {
+    let santiagoDateElement = santiagoElement.querySelector(".date");
+    let santiagoTimeElement = santiagoElement.querySelector(".time");
+    let santiagoTime = moment().tz("America/Santiago");
 
-    munichDateElement.innerHTML = munichTime.format("MMMM	Do YYYY");
-    munichTimeElement.innerHTML = munichTime.format(
+    santiagoDateElement.innerHTML = santiagoTime.format("MMMM	Do YYYY");
+    santiagoTimeElement.innerHTML = santiagoTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+}
+
+  function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz("#cities");
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
+    updateTime();
+    setInterval(updateTime, 1000);
+
+    let citiesSelectElement = document.querySelector("#city");
+    citiesSelectElement.addEventListener("change", updateCity);
