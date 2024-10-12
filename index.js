af81ebc76a6d435f1467b54ce1fb6f2dc3dfa61c
Santiey <santanamuchena@gmail.com>
2024-10-12 22:28:39 +0200

    updated js

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ let mutareElement = document.querySelector("#mutare");
 
   function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+    }
+
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz("#cities");
     let citiesElement = document.querySelector("#cities");
